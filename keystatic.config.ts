cce688ed4786f16ea057b433efd571aa725ad78a
filipsidak-arch <filip.sidak@gmail.com>
2026-04-25 23:38:11 +0200

    Keystatic přepnut na GitHub mode

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,7 +3,8 @@ import { config, fields, collection, singleton } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: 'filipsidak-arch/stackup-studio-nextjs',
   },
   collections: {
     references: collection({
